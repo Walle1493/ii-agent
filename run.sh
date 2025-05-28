42cf6997cd80792d44d1f2631abc42dfb416c9c8
p_mxingdong <p_mxingdong@tencent.com>
2025-05-28 17:39:02 +0800

    modify model prototype

---
+++ run.sh
@@ -0,0 +1,4 @@
+export NEXT_PUBLIC_API_URL=http://0.0.0.0:8000
+export NEXT_PUBLIC_VSCODE_URL=http://127.0.0.1:8080
+export STATIC_FILE_BASE_URL=http://localhost:8000
+python ws_server.py --port 8000
